b9cb0e554f6f4fad2e4404a39b5a34571f9732fa
Matt Carey <mattca@users.noreply.github.com>
2018-05-30 17:57:57 +0100

    add failing test for multiple items

--- src/test/java/com/itv/CheckoutTest.java
+++ src/test/java/com/itv/CheckoutTest.java
@@ -45,4 +45,22 @@ public class CheckoutTest {
         assertEquals(IS_EQUAL, BigDecimal.ZERO.compareTo(totals.getSubTotal()));
         assertEquals(IS_EQUAL, BigDecimal.ZERO.compareTo(totals.getTotal()));
     }
+
+    @Test
+    public void testCheckoutTotalsWithMultipleItemsNoReductions() {
+        // given
+        Checkout checkout = new Checkout();
+        Item itemC = new Item("C", BigDecimal.valueOf(0.2), null);
+        Item itemD = new Item("D", BigDecimal.valueOf(0.15), null);
+
+        // when
+        checkout.scanItem(itemC);
+        checkout.scanItem(itemD);
+        Totals totals = checkout.calculateTotals();
+
+        // then
+        assertEquals(IS_EQUAL, BigDecimal.ZERO.compareTo(totals.getReductions()));
+        assertEquals(IS_EQUAL, BigDecimal.valueOf(0.35).compareTo(totals.getSubTotal()));
+        assertEquals(IS_EQUAL, BigDecimal.valueOf(0.35).compareTo(totals.getTotal()));
+    }
 }
