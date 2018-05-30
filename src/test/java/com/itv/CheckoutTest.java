9c63ba37b9c001cc3e39ed3bf986de54ae409ec4
Matt Carey <mattca@users.noreply.github.com>
2018-05-30 21:42:20 +0100

    add failing test for item with reductions

--- src/test/java/com/itv/CheckoutTest.java
+++ src/test/java/com/itv/CheckoutTest.java
@@ -63,4 +63,22 @@ public class CheckoutTest {
         assertEquals(IS_EQUAL, BigDecimal.valueOf(0.35).compareTo(totals.getSubTotal()));
         assertEquals(IS_EQUAL, BigDecimal.valueOf(0.35).compareTo(totals.getTotal()));
     }
+
+    @Test
+    public void testCheckoutTotalsWithAnItemWithReductions() {
+        // given
+        Checkout checkout = new Checkout();
+        Item itemB = new Item("B", BigDecimal.valueOf(0.3), BUY_ONE_GET_SECOND_HALF_PRICE);
+
+        // when
+        checkout.scanItem(itemB);
+        checkout.scanItem(itemB);
+        checkout.scanItem(itemB);
+        Totals totals = checkout.calculateTotals();
+
+        // then
+        assertEquals(IS_EQUAL, BigDecimal.valueOf(0.15).compareTo(totals.getReductions()));
+        assertEquals(IS_EQUAL, BigDecimal.valueOf(0.90).compareTo(totals.getSubTotal()));
+        assertEquals(IS_EQUAL, BigDecimal.valueOf(0.75).compareTo(totals.getTotal()));
+    }
 }
