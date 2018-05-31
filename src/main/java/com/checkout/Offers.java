918dc982bd212e653fc360dae27655e5a6ab94f0
Matt Carey <mattca@users.noreply.github.com>
2018-05-31 22:45:36 +0100

    javadoc

--- src/main/java/com/checkout/Offers.java
+++ src/main/java/com/checkout/Offers.java
@@ -6,18 +6,25 @@ import java.util.Map.Entry;
 import java.util.Optional;
 
 /**
- * Class to register that an item has a particular multibuy offer
+ * Class used to register offers
  */
 public class Offers {
 
     private final Map<Item, MultibuyOffer> offers = new HashMap<>();
 
+    /**
+     * Register that an item has a particular multibuy offer
+     *
+     * @param item          Item which has an offer
+     * @param multibuyOffer Offer to relate to the item
+     */
     public void registerOffer(Item item, MultibuyOffer multibuyOffer) {
         offers.put(item, multibuyOffer);
     }
 
     /**
      * Retrieves details of an offer given an item SKU
+     *
      * @param sku Item SKU
      * @return Optional of the offer. Empty optional if no offer found for that SKU
      */
