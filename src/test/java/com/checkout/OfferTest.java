12136baa69682b732953108385a6a005a34e4785
Matt Carey <mattca@users.noreply.github.com>
2018-05-31 23:10:18 +0100

    add valid offer test and two failing tests for passing negative percent off

--- src/test/java/com/checkout/OfferTest.java
+++ src/test/java/com/checkout/OfferTest.java
@@ -4,6 +4,8 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
+import static org.junit.Assert.assertEquals;
+
 public class OfferTest {
 
     @Rule
@@ -16,6 +18,42 @@ public class OfferTest {
 
         // when
         Offer offer = new Offer(0, 50);
+
+        // then
+        // exception is thrown
+    }
+
+    @Test
+    public void checkValidOfferPopulatesCorrectValues() {
+        // given
+
+        // when
+        Offer offer = new Offer(2, 100);
+
+        // then
+        assertEquals(2, offer.getQuantityToEnableReduction());
+        assertEquals(100, offer.getReducedItemPercentOff());
+    }
+
+    @Test
+    public void checkIllegalArgumentExceptionThrownWhenPassingNegativeReducedItemPercentOff() {
+        // given
+        exception.expect(IllegalArgumentException.class);
+
+        // when
+        Offer offer = new Offer(1, -1);
+
+        // then
+        // exception is thrown
+    }
+
+    @Test
+    public void checkIllegalArgumentExceptionThrownWhenPassingReducedItemPercentOffOver100() {
+        // given
+        exception.expect(IllegalArgumentException.class);
+
+        // when
+        Offer offer = new Offer(1, 101);
 
         // then
         // exception is thrown
