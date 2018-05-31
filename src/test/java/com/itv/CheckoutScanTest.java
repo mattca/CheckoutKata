c1b007060efcecc8343537fc101fca4bea1957cb
Matt Carey <mattca@users.noreply.github.com>
2018-05-31 15:44:24 +0100

    tidy tests, start tests using items with no reductions

--- src/test/java/com/itv/CheckoutScanTest.java
+++ src/test/java/com/itv/CheckoutScanTest.java
@@ -19,7 +19,7 @@ public class CheckoutScanTest {
     public void scanItemShouldAddToCheckoutItems() {
         // given
         Checkout checkout = new Checkout();
-        Item item = new Item("A", BigDecimal.valueOf(0.5), BUY_TWO_GET_40_PC_OFF_THIRD_ITEM);
+        Item item = new Item("C", BigDecimal.valueOf(0.2));
 
         // when
         checkout.scanItem(item);
@@ -34,7 +34,7 @@ public class CheckoutScanTest {
     public void scanMultipleItemsOfSameTypeShouldAddToCheckoutItems() {
         // given
         Checkout checkout = new Checkout();
-        Item item = new Item("A", BigDecimal.valueOf(0.5), BUY_TWO_GET_40_PC_OFF_THIRD_ITEM);
+        Item item = new Item("C", BigDecimal.valueOf(0.2));
 
         // when
         checkout.scanItem(item);
@@ -51,19 +51,19 @@ public class CheckoutScanTest {
     public void scanMultipleItemsOfVariousTypesShouldAddToCheckoutItems() {
         // given
         Checkout checkout = new Checkout();
-        Item itemA = new Item("A", BigDecimal.valueOf(0.5), BUY_TWO_GET_40_PC_OFF_THIRD_ITEM);
+        Item itemC = new Item("C", BigDecimal.valueOf(0.2));
         Item itemB = new Item("B", BigDecimal.valueOf(0.3), BUY_ONE_GET_SECOND_HALF_PRICE);
 
         // when
-        checkout.scanItem(itemA);
-        checkout.scanItem(itemA);
+        checkout.scanItem(itemC);
+        checkout.scanItem(itemC);
         checkout.scanItem(itemB);
         checkout.scanItem(itemB);
 
         // then
-        assertTrue(checkout.getCheckoutItems().containsKey(itemA));
+        assertTrue(checkout.getCheckoutItems().containsKey(itemC));
         assertTrue(checkout.getCheckoutItems().containsKey(itemB));
-        assertEquals(Integer.valueOf(2), checkout.getCheckoutItems().get(itemA));
+        assertEquals(Integer.valueOf(2), checkout.getCheckoutItems().get(itemC));
         assertEquals(Integer.valueOf(2), checkout.getCheckoutItems().get(itemB));
         assertEquals(2, checkout.getCheckoutItems().size());
         assertEquals(4, checkout.getTotalItemsInCheckout());
